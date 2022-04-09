bf44d52afc6e18fa97829f318553f890ae2e1c9c
kenneth-okoro <kennethokesiean@gmail.com>
2022-04-09 23:30:48 +0100

    added initial files

---
+++ index.js
@@ -0,0 +1,29 @@
+const icon = document.getElementById('icon');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navLink = document.querySelectorAll('.nav-link');
+
+icon.addEventListener('click', toggleMode);
+
+function toggleMode() {
+  document.body.classList.toggle('light-theme');
+  if (document.body.classList.contains('light-theme')) {
+    icon.src = './images/moon.png';
+  } else {
+    icon.src = './images/sun.png';
+  }
+}
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
